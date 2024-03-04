5f2cc274ed4c6ceab1b98be20c0ac3c023318587
iwon <67680031+DevIwon@users.noreply.github.com>
2024-03-04 16:36:29 +0900

    feat: 커스텀 알림창 작업 완료

---
+++ Dabie-iOS/Dabie-iOS/Controllers/AlertVC.swift
@@ -0,0 +1,32 @@
+//
+//  AlertVC.swift
+//  Dabie-iOS
+//
+//  Created by 서원진 on 2024/03/04.
+//
+import SnapKit
+import UIKit
+
+
+class AlertVC: UIViewController {
+  
+    //MARK: - Views
+    private var alertView: AlertView!
+    //MARK: - Properties
+    
+    //MARK: - Life Cycles
+    override func loadView() {
+        super.loadView()
+        
+        alertView = AlertView(frame: self.view.frame)
+        self.view = alertView
+       
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Dd")
+
+    }
+    
+}
